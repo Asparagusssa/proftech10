02cebc7e59ca1c981d5c1d0a23bd6ad458d7791c
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-01-29 23:55:10 +0400

    update

--- app/Http/Controllers/EmailEmailTypeController.php
+++ app/Http/Controllers/EmailEmailTypeController.php
@@ -11,18 +11,26 @@ class EmailEmailTypeController extends Controller
     public function attach($email_id, $email_type_id)
     {
         $email = Email::findOrFail($email_id);
-        $email->types()->syncWithoutDetaching($email_type_id);
+        if ($email->types()->syncWithoutDetaching($email_type_id)) {
+            return response()->json([
+                'message' => 'Email привязан'
+            ]);
+        }
         return response()->json([
-            'message' => 'Email привязан'
+            'message' => 'Ошибка'
         ]);
     }
 
     public function detach($email_id, $email_type_id)
     {
         $email = Email::findOrFail($email_id);
-        $email->types()->detach($email_type_id);
+        if($email->types()->detach($email_type_id)) {
+            return response()->json([
+                'message' => 'Email отвязан'
+            ]);
+        }
         return response()->json([
-            'message' => 'Email отвязан'
+            'message' => 'Ошибка'
         ]);
     }
 
